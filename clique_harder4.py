aca93cfb0aa3edf1fe1419a7fd68c33c72bd2228
Aurojit Panda <apanda@cs.berkeley.edu>
2012-03-06 19:21:43 -0800

    Non planar graphs

--- clique_harder4.py
+++ clique_harder4.py
@@ -89,11 +89,11 @@ def CheckTables(tables, size, clique, degrees, k):
             print  str.format("{0}", table)
 
 if __name__ == "__main__":
-    clique = [(0,1),(0,2),(0,3),(0,4),(1,2),(3,4),(1,4)]
-    degrees = [4, 3, 2, 2, 3]
-    neighbours = {0:[0,1,2,3,4], 1:[0,1,2,4],2:[0,1,2],3:[0,3,4],4:[0,1,3,4]}
+    clique = [(0,1),(0,2),(0,4),(0,5),(1,2),(2,3),(2,5),(3,4),(4,5)]
+    degrees = [4, 2, 4, 2, 3, 3]
+    neighbours = {0:[0,1,2,4,5], 1:[0,1,2],2:[0,1,2,3,5],3:[2,3,4],4:[0,3,4,5],5:[0,2,4,5]}
     if len(sys.argv) <= 1:
-        CheckTables(CombineRouteIterators(5, neighbours), 5,clique,degrees,2)
+        CheckTables(CombineRouteIterators(6, neighbours), 6,clique,degrees,2)
     else:
         f = open(sys.argv[1])
         for line in f:
